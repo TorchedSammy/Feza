d724fd70354a7215a4962d6bf84dc22c8070789b
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-08-20 14:42:58 -0400

    feat: add lua global vars

--- lua.go
+++ lua.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/user"
+	"runtime"
 
 	"github.com/arnodel/golua/lib"
 	"github.com/arnodel/golua/lib/debuglib"
@@ -22,6 +24,24 @@ func initLua() {
 	lib.LoadLibs(r, systemLoader)
 	lib.LoadLibs(r, processLoader)
 	lib.LoadLibs(r, regexLoader)
+
+	// Globals
+	var platform string
+	switch runtime.GOOS {
+		case "windows": platform = "Windows"
+		case "darwin": platform = "Mac OS X"
+		case "linux": platform = "Linux"
+		// TODO: ios and android (will lavatera even build on those platforms..?)
+	}
+	exe, _ := os.Executable()
+	curuser, _ := user.Current()
+	homedir := curuser.HomeDir
+
+	r.SetEnv(env, "ARGS", rt.TableValue(rt.NewTable())) // TODO: convert os.Args to lua
+	r.SetEnv(env, "PLATFORM", rt.StringValue(platform))
+	r.SetEnv(env, "SCALE", rt.IntValue(1)) // TODO: get dpi
+	r.SetEnv(env, "EXEFILE", rt.StringValue(exe))
+	r.SetEnv(env, "HOME", rt.StringValue(homedir))
 
 	err := doFile(r, "init.lua")
 	fmt.Println(err)
