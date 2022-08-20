598f6ff58695c1e2fc2f97441f938e598a20c953
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-08-20 18:41:55 -0400

    feat: run lite xl code
    milestone!! we have now reached the point
    where we can actually get started to running the editor
    woo hoo

--- init.lua
+++ init.lua
@@ -1,2 +1,32 @@
-package.path = package.path .. ';./data/?.lua'
-dofile 'data/core/init.lua'
+local core
+xpcall(function()
+	local exedir = EXEFILE:match('^(.*)/[^/]+$')
+	local prefix = exedir:match('^(.*)[^/]+bin$')
+	dofile((MACOS_RESOURCES or (prefix and prefix .. '/share/lite-xl' or exedir .. '/data')) .. '/core/start.lua')
+	core = require(os.getenv 'LITE_XL_RUNTIME' or 'core')
+	core.init()
+	core.run()
+end, function(err)
+	local error_dir
+	io.stdout:write('Error: '..tostring(err)..'\n')
+	io.stdout:write(debug.traceback(nil, 4)..'\n')
+	io.flush()
+
+	if core and core.on_error then
+		error_dir = USERDIR
+		pcall(core.on_error, err)
+	else
+		error_dir = system.absolute_path '.'
+		local fp = io.open('error.txt', 'wb')
+		fp:write('Error: ' .. tostring(err) .. '\n')
+		fp:write(debug.traceback(nil, 4)..'\n')
+	end
+
+	fp:close()
+	system.show_fatal_error('Lite XL internal error',
+		'An internal error occurred in a critical part of the application.\n\n'..
+		'Please verify the file "error.txt" in the directory '..error_dir)
+	os.exit(1)
+end)
+
+return core and core.restart_request
